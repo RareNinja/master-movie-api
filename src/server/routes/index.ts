62cab58384e1088065b9226aba25f93a8d8fbea8
davi.balan <90470403+RareNinja@users.noreply.github.com>
2023-09-29 19:28:02 -0300

    commit test originalMovies

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { Request, Response } from "express";
+import { fetchOriginalsMovies } from "../../services";
 
 const router = Router();
 
 router.get("/originalMovies", async (req: Request, res: Response) => {
     console.log(req);
     try {
-        // const originalMovies = await fetchOriginalsMovies();
-        // if (originalMovies) {
-        res.status(200).send("Bompas não ira na pizza amanha");
-        // }
+        const originalMovies = await fetchOriginalsMovies();
+        if (originalMovies) {
+            res.status(200).json(originalMovies);
+        }
     } catch (error: any) {
         res.status(400).json({ message: error.message, });
     }
